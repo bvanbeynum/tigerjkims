0410e3dde0f8eab715f687a9b318cf38dd5a91c2
Brett van Beynum <maildrop444@gmail.com>
2021-08-24 19:06:04 -0400

    Changed phone number

--- web/server/routes.js
+++ web/server/routes.js
@@ -46,10 +46,45 @@ module.exports = function (app) {
 	
 	app.post("/sendmail", function (request, response) {
 		var dateFormat = new Date();
-		dateFormat = dateFormat.getMonth() + "/" + dateFormat.getDate() + "/" + dateFormat.getYear() + " " + dateFormat.getHours() + ":" + dateFormat.getMinutes();
+		dateFormat = dateFormat.getMonth() + "/" + dateFormat.getDate() + "/" + dateFormat.getFullYear() + " " + dateFormat.getHours() + ":" + dateFormat.getMinutes();
 		
-		var mailOptions = {
-			from: "\"Website inquiry\" <tigerjkimstkd@gmail.com>",
+		// var mailOptions = {
+		// 	from: "\"Website inquiry\" <tigerjkimstkd@gmail.com>",
+		// 	to: "tigerjkimstkd@gmail.com",
+		// 	subject: dateFormat + ": New Website Inquiry",
+		// 	generateTextFromHTML: true,
+		// 	html: "<p><b>There was a new website inquiry sent at " + dateFormat + "</b></p>" +
+		// 		"<p>" +
+		// 		"Name: " + request.body.name + "<br>" +
+		// 		"Email: " + request.body.email + "<br>" +
+		// 		"Phone: " + request.body.phone + "<br>" +
+		// 		"Comments: " + request.body.comments +
+		// 		"</p>"
+		// };
+		
+		// smtpTraservicensport.sendMail(mailOptions, function(error, info){
+		// 	if(error){
+		// 		console.log(error);
+		// 		response.send("error");
+		// 	}
+		// 	else {
+		// 		response.send("ok");
+		// 	}
+		// });
+
+		var service = nodemailer.createTransport({
+			host: "smtp.gmail.com",
+			port: 465,
+			secure: true,
+			auth: {
+				type: "OAuth2",
+				clientId: "743032936512-vpikma7crc8dssoah9bv1la06s2sl4a2.apps.googleusercontent.com",
+				clientSecret: "EGD193Mwf6kO798wdP9Bq7lf"
+			}
+		});
+		
+		var options = {
+			from: "\"Website inquiry\" <bvanbeynum@gmail.com>",
 			to: "tigerjkimstkd@gmail.com",
 			subject: dateFormat + ": New Website Inquiry",
 			generateTextFromHTML: true,
@@ -59,13 +94,20 @@ module.exports = function (app) {
 				"Email: " + request.body.email + "<br>" +
 				"Phone: " + request.body.phone + "<br>" +
 				"Comments: " + request.body.comments +
-				"</p>"
+				"</p>",
+			auth: {
+				user: "bvanbeynum@gmail.com",
+				refreshToken: "1//04K4dB_Z_X1rQCgYIARAAGAQSNwF-L9Irhjcc5YawPcBGv-zZuBiZHm2-s3bgPEJf6VQm6b9eTs7E4iuRbUij6-tzAVYi_3ZXbVU",
+				accessToken: "ya29.a0AfH6SMCf0nD3px4QPS-MABYUSpsEEPPdOGAJkvCfOE5eMiuBIUPw-EWunj6wsbEXtJthE16v02r6VWhdcjOaUEmqGFQsD7iEZR26h4B8Lzfh-NAw2OjpfApxfjNz5NEv-JAT6kBTA4J7G2rntClDhTxanW-6_s2y528",
+				expires: 3460
+			}
 		};
 		
-		smtpTransport.sendMail(mailOptions, function(error, info){
-			if(error){
+		service.sendMail(options, (error, mailResponse) => {
+			if (error) {
 				console.log(error);
 				response.send("error");
+				return;
 			}
 			else {
 				response.send("ok");
